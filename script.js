852550496832a0c1d6336cc031d863ceee7fb9c5
Robert Ewing <40344772@live.napier.ac.uk>
2020-03-19 14:06:56 +0000

    inital commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function swornPromise() {
+  alert('Sworn Promise Rules: \nRule 1: You can not interfere in the development of a planet including the development of the alien life/society. \nRule 2: You must try and communicate with a unidentified ship before attacking. \nRule 3: You must not allow non space explorers on the ship. \nRule 4: If your ship is being taking over you must abandon ship if the threat cannot be contained. \nRule 5:');
+}
+
+function saveCookie(){
+
+}
+
+function loadCookie(){
+
+}
